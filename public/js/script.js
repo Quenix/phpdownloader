8123bac29c8a2ec1d570916782ad60d8e9d1fd37
Leonardo Vasques <quenixserra@gmail.com>
2018-12-10 05:50:46 -0200

    captura thumbnail

--- public/js/script.js
+++ public/js/script.js
@@ -5,10 +5,8 @@ $(document).ready( function () {
 	});
 });
 
-function downloadModal(videos){
-	
-	console.log(videos);
-	
+function downloadModal(videos, thumbnail){
+
 	var html = "";
 	
 	for(var video in videos){
@@ -17,23 +15,36 @@ function downloadModal(videos){
 	}
 	
 	$.alert({
-	    title: 'Alert!',
-	    content: html,
+	    title: videos[0].title,
+	    content: "<img src="+thumbnail+">"+html,
 	});
+}
+
+function helperExtractIdFromUrl(url){
+    var video_id = url.split('v=')[1];
+    var ampersandPosition = video_id.indexOf('&');
+    if(ampersandPosition != -1) {
+        video_id = video_id.substring(0, ampersandPosition);
+    }
+
+    var thumbnail = "https://img.youtube.com/vi/"+video_id+"/0.jpg";
+
+    return thumbnail;
 }
 
 
 function ajaxDownloader(){
 	
 	var data = $("#youtubeDownloadField").val();
-	
+	var thumbnail = helperExtractIdFromUrl(data);
+
 	$.ajax({
 	    type: "POST",
 	    url: "controller/Downloader.php",
 	    data: {data:data},
 	    dataType: "json",
 	    success: function(response) {
-	    	downloadModal(response);
+	    	downloadModal(response, thumbnail);
 	    }
 	});
 	
