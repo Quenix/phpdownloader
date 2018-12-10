5add09d32237c5e601bc62feecc0088134544200
Leonardo Vasques <quenixserra@gmail.com>
2018-12-10 03:34:58 -0200

    primeira view

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
 require_once('vendor/autoload.php');
+require_once('public/home.php');
 
 $yt = new YouTubeDownloader();
 
-$links = $yt->getDownloadLinks("https://www.youtube.com/watch?v=eVTXPUF4Oz4");
+//$links = $yt->getDownloadLinks("https://www.youtube.com/watch?v=eVTXPUF4Oz4");
 
-var_dump($links);
-
-die('foi!');
+//var_dump($links);
